a7f32806596113b24f4522ce9dc74b00eddfeca5
Anton Garifullin <garifullin.antoshka@mail.ru>
2022-07-04 23:45:14 +0700

    ui dialog component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="app">
-    <PostForm
-        @create="createPost"
-    />
+    <h1>Страница с постами</h1>
+    <my-button @click="dialogVisible = true">Создать пост</my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <PostForm
+          @create="createPost"
+      />
+    </my-dialog>
     <PostList
         @remove="removePost"
         :posts="posts"
@@ -13,9 +17,11 @@
 <script>
 import PostList from "@/components/posts/PostList";
 import PostForm from "@/components/posts/PostForm";
+import MyDialog from "@/components/UI/MyDialog";
 
 export default {
   components: {
+    MyDialog,
     PostForm, PostList
   },
   data() {
@@ -25,11 +31,13 @@ export default {
         {id: 2, title: 'title2', body: 'description of post 2'},
         {id: 3, title: 'title3', body: 'description of post 3'},
       ],
+      dialogVisible: false,
     }
   },
   methods: {
     createPost(post, ...args){
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter(p => p.id !== post.id);
