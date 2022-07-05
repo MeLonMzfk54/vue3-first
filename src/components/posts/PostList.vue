06bda3e0adc6a91d7773a07fbfb0c5a920624412
Anton Garifullin <garifullin.antoshka@mail.ru>
2022-07-05 15:08:09 +0700

    list animation

--- src/components/posts/PostList.vue
+++ src/components/posts/PostList.vue
@@ -1,12 +1,14 @@
 <template>
   <div v-if="posts.length">
     <h3>Список постов</h3>
-    <post-item
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-    />
+    <transition-group name="post-list">
+      <post-item
+          v-for="post in posts"
+          :post="post"
+          :key="post.id"
+          @remove="$emit('remove', post)"
+      />
+    </transition-group>
   </div>
   <h2 v-else style="color: red">
     Список постов пуст
@@ -28,4 +30,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  .post-list {
+    &-item {
+      display: inline-block;
+      margin-right: 10px;
+    }
+    &-enter-active,
+    &-leave-active {
+      transition: all 1s ease;
+    }
+    &-enter-from,
+    &-leave-to {
+      opacity: 0;
+      transform: translateY(30px);
+    }
+    &-move {
+      transition: transform 0.8s ease;
+    }
+  }
 </style>
